cd86e5b31390ca96d30eb9d20f6d302b039780f3
Hansee <hansee1996@gmail.com>
2023-01-27 20:12:50 -0500

    Se refactoriza el juego de tenis 3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -1,6 +1,7 @@
+import utils.Score;
 
 public class TennisGame3 implements TennisGame {
-    
+
     private int p2;
     private int p1;
     private String p1N;
@@ -12,25 +13,47 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
-            String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"}; 
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-        } else {
-            if (p1 == p2)
-                return "Deuce";
-            s = p1 > p2 ? p1N : p2N;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+        if (isGameBelowFortyPoints(p1, p2) && noDeuceWinningScore(p1, p2)) {
+            return belowFortyPointsCalculationScore(p1, p2);
         }
+        return isDeuceResult(p1, p2) ? "Deuce" : overDeuceResult(p1, p2);
     }
-    
+
+    private boolean isGameBelowFortyPoints(int player1Score, int player2Score) {
+        return player1Score < 4 && player2Score < 4;
+    }
+
+    private boolean noDeuceWinningScore(int player1Score, int player2Score) {
+        return player1Score + player2Score != 6;
+    }
+
+    private boolean isAdvantagePoint(int player1Score, int player2Score) {
+        return Math.abs(player1Score - player2Score) == 1;
+    }
+
+    private boolean isDeuceResult(int player1Score, int player2Score) {
+        return player1Score == player2Score;
+    }
+
+    private String overDeuceResult(int player1Score, int player2Score) {
+        String playerWinning = player1Score > player2Score ? p1N : p2N;
+        return isAdvantagePoint(player1Score, player2Score) ?
+                "Advantage " + playerWinning : "Win for " + playerWinning;
+    }
+
+    private String regularGameScoreResult(int tempScore) {
+        return Score.values()[tempScore].getScore();
+    }
+
+    private String belowFortyPointsCalculationScore(int player1Score, int player2Score) {
+        return (player1Score == player2Score) ?
+                regularGameScoreResult(p1) + "-All" : regularGameScoreResult(p1) + "-" + regularGameScoreResult(p2);
+    }
+
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
+        if ("player1".equals(playerName))
             this.p1 += 1;
         else
             this.p2 += 1;
-        
     }
-
 }
